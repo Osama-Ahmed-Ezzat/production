cfb2c1c18a94dd07bc7b272b6e8206d499aa0b92
surgi-tech <59487271+surgi-tech@users.noreply.github.com>
2020-06-08 11:55:40 +0200

    surgi_analytic_account

--- surgi_analytic_account/models/models.py
+++ surgi_analytic_account/models/models.py
@@ -6,6 +6,7 @@ class AccountAnalyticAccount(models.Model):
     user_id = fields.Many2one(comodel_name="res.users", string="Salesperson",)
     salesteam_id = fields.Many2one(comodel_name="crm.team", string="Sales Team",)
     product_id = fields.Many2one(comodel_name="product.lines", string="Product Line",)
+    undefined_sales_person = fields.Boolean(string="Undefined Sales Person",  )
 
 
 class ProductProduct(models.Model):
@@ -21,24 +22,21 @@ class AccountMove(models.Model):
     # @api.onchange('is_check')
     def compute_analytic_account(self):
 
-        analytic_account_obj=self.env['account.analytic.account'].search([('user_id','=',self.invoice_user_id.id)])
-
+        analytic_account_obj=self.env['account.analytic.account'].search([])
         for line in self.invoice_line_ids:
-            print("11111111111111111111111111111111")
-            if analytic_account_obj:
-                for rec in analytic_account_obj:
-                    print("2222222222222222222222222222222222222")
-                    if line.product_id.product_id==rec.product_id:
-                        print("33333333333333333333333333333")
+            part=False
+            for rec in analytic_account_obj:
+                if line.product_id.product_id==rec.product_id:
+                    part=True
+                    if rec.user_id == self.invoice_user_id:
                         line.analytic_account_id=rec.id
-                        self.is_check=True
+                    elif rec.undefined_sales_person==True:
+                        line.analytic_account_id = rec.id
+                    else:
+                        line.analytic_account_id=False
+                if part==False:
+                    line.analytic_account_id = False
 
-                    elif line.product_id.product_id==rec.product_id:
-                        line.analytic_account_id =False
-                        self.is_check = False
-            else:
-                line.analytic_account_id = False
-                self.is_check = False
 
 
 
